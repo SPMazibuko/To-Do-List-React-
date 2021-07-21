23bf180e9baef2fc4ad5b3184c76f327ff2ff224
SPMazibuko <spmazibuko07@gmail.com>
2021-07-21 10:41:27 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -35,68 +35,92 @@ if(!firebase.apps.length){
 }
 
 const useStyles = makeStyles((theme) => ({
-  
+      root:{
+
+        display: 'flex',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+    },
   paperSyle: {
-    padding: theme.spacing(2),
     backgroundImage: "url(" + "https://scontent.fjnb11-1.fna.fbcdn.net/v/t1.6435-9/218928331_6101479296591986_1474258448850844212_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeFEOvTLaX7BLPghEYrUysJdAK9IkqXyckAAr0iSpfJyQFFQjRsgu2Xu5Y19ViDrc7BUrDf6yP6cCUMkPeUWAF6u&_nc_ohc=CwhSAxKVnsMAX-_pyfO&_nc_ht=scontent.fjnb11-1.fna&oh=0d26aba78b31de2b13d4e44de6c516b2&oe=60FC3CB1" + ")",
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat'
+    color: '#0000000'
   },
   button: {
     margin: theme.spacing(1),
-  }
+  },
+  image: {
+    backgroundImage: 'url(https://scontent.fjnb11-1.fna.fbcdn.net/v/t1.6435-9/218230350_6101479503258632_6092955877124397050_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeEFa4M1tk0O-rvUgNMiEp8YWkTzqeHoTLBaRPOp4ehMsPxFGP31U8vyn4FAcMj3gIC-HQHFOzuBcfQ6Amq0a2C5&_nc_ohc=GCvAhaJeFCsAX_LqbjE&tn=6VvZ5GfI6aYD5Tzn&_nc_ht=scontent.fjnb11-1.fna&oh=e6177222a48f69dfcb3c3c2488cf9dfe&oe=60FD54B2)',
+      width: '700px',
+      height: '600px',
+      overflow: 'visible',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'left',
+    paddingLeft: 30,
+  },
 
 }));
 
 export default function App() {
   const classes = useStyles();
   return (
-    <Router>
-      <Grid id="main">
-        <Paper elevation={20} className={classes.paperSyle} id="mainPaper" >
-          <Grid id="formSetting" style={{ padding: '20px 20px', width: 587, backgroundColor: '#ffffff', overflow: 'hidden',
-  borderRadius: '20px', textAlign: 'center', }}>
-            <h2 style={{overflow: 'visible', whiteSpace: 'pre',
-fontSize: '46px', letterSpacing: '-2px', color: '#333', lineHeight: '1.1', fontWeight: 700, fontStyle: 'normal', fontFamily: 'Arvo, serif', margin:0, }}>
-  Create An Account
-  </h2>
-    <Typography variant='caption'>
-      Please fill up all fields in this form
-    </Typography>
-    
-    <Grid align='center'>
-    <form>
+    <Router> 
+      <Grid id="main" >
+        <Paper elevation={20} className={classes.root} id="mainPaper" >
+          <Grid id="formSetting" style={{ padding: '20px 20px', width: 587, backgroundColor: '#ffffff', overflow: 'hidden', borderRadius: '20px', textAlign: 'center', }} >
+          <Grid container spacing={1}>
+            <Paper     elevation ={0} className={classes.paper} id="left">
+             <Grid item xs={6} >
 
-    <TextField variant="outlined" margin="normal" required fullWidth id="email" placeholder="Enter Email address" name="email" placeholderColor="rgb(170, 170, 170)" padding={44} radius={8} backgroundColor="rgb(245, 245, 245)" label="Email Address:" />
+             <Grid align='center'>
+                <h2 style={{overflow: 'visible', whiteSpace: 'pre', fontSize: '46px', letterSpacing: '-2px', color: '#333', lineHeight: '1.1', fontWeight: 700, fontStyle: 'normal', fontFamily: 'Arvo, serif', margin:0, }}>
+                Create An Account
+                </h2>
 
-    <TextField variant="outlined" margin="normal" required fullWidth id="nm" placeholder="Enter Your Name" name="nm" placeholderColor="rgb(170, 170, 170)" padding={44} radius={8} backgroundColor="rgb(245, 245, 245)" label="Name:" />
+                <Typography variant='caption'>
+                   Please fill up all fields in this form
+                </Typography>
+
+                
+                  <form>
+
+                    <TextField variant="outlined" margin="normal" required fullWidth id="email" placeholder="Enter Email address" name="email" placeholderColor="rgb(170, 170, 170)" padding={44} radius={8} backgroundColor="rgb(245, 245, 245)" label="Email Address:" />
+
+                    <TextField variant="outlined" margin="normal" required fullWidth id="nm" placeholder="Enter Your Name" name="nm" placeholderColor="rgb(170, 170, 170)" padding={44} radius={8} backgroundColor="rgb(245, 245, 245)" label="Name:" />
      
-    <TextField variant="outlined" margin="normal" required fullWidth name="password"  placeholder="Create Password" type="password" id="password" />
+                    <TextField variant="outlined" margin="normal" required fullWidth name="password"  placeholder="Create Password" type="password" id="password" />
 
-<TextField variant="outlined" margin="normal" required fullWidth name="password"  placeholder="Confirm Password" type="password" id="password" />
+                    <TextField variant="outlined" margin="normal" required fullWidth name="password"  placeholder="Confirm Password" type="password" id="password" />
 
-<FormControlLabel control={<Checkbox name="checkedB" color="primary"
-fullWidth/>} label="Accept Terms & Conditions" />
+                    <FormControlLabel control={<Checkbox name="checkedB" color="primary" fullWidth variant="contained"/>} label="Accept Terms & Conditions" />
        
-<Button type='submit' color="primary" variant="contained" className={classes.button} startIcon={<ExitToAppRoundedIcon />}
-      fullWidth>
-        SIGN UP
-      </Button>
-    </form>
-    <Grid container>
-                        <Grid item xs>
-                        </Grid>
+                    <Button type='submit' color="primary" variant="contained" className={classes.button} startIcon={<ExitToAppRoundedIcon />} fullWidth>
+                      SIGN UP
+                    </Button>
+                  </form>
+                  <Grid container>
+                    <Grid item xs>
                       <Grid item>
                         <Link href="#" variant="body2">
-                         {"Already have an account? Sign Up"}
+                          {"Already have an account? Sign Up"}
                         </Link>
                       </Grid>
-                      </Grid>
-    </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
-        </Paper>
-      </Grid>
+           </Grid>
+        <Grid item xs={6}>
+          <div  className={classes.image}/>
+        </Grid>
+      </Paper>
+    </Grid>
     </Router>
   );
 }
