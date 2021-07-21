12b019442999ccd6cfbe4a96872ca9efbbf705a0
SPMazibuko <spmazibuko07@gmail.com>
2021-07-21 06:56:08 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import {Grid, 
+  Paper,
+  Typography,
+TextField,
+Button,
+}from '@material-ui/core';
+import EmailRoundedIcon from '@material-ui/icons/EmailRounded';
 import {
   BrowserRouter as Router,
   Switch,
@@ -31,9 +36,10 @@ const useStyles = makeStyles((theme) => ({
   
   paperSyle: {
     padding: theme.spacing(2),
-    textAlign: 'left',
-    color: '#000000',
-    paddingLeft: 30,
+    backgroundImage: "url(" + "https://scontent.fjnb11-1.fna.fbcdn.net/v/t1.6435-9/218928331_6101479296591986_1474258448850844212_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeFEOvTLaX7BLPghEYrUysJdAK9IkqXyckAAr0iSpfJyQFFQjRsgu2Xu5Y19ViDrc7BUrDf6yP6cCUMkPeUWAF6u&_nc_ohc=CwhSAxKVnsMAX-_pyfO&_nc_ht=scontent.fjnb11-1.fna&oh=0d26aba78b31de2b13d4e44de6c516b2&oe=60FC3CB1" + ")",
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat'
   },
 
 }));
@@ -42,9 +48,33 @@ export default function App() {
   const classes = useStyles();
   return (
     <Router>
-      <Grid>
-        <Paper elevation={20} className={classes.paperSyle}>
-          <h2>Create An Acount</h2>
+      <Grid id="main">
+        <Paper elevation={20} className={classes.paperSyle} id="mainPaper" >
+          <Grid id="formSetting" style={{ padding: '20px 20px', width: 587, backgroundColor: '#ffffff', overflow: 'hidden',
+  borderRadius: '20px', textAlign: 'center', }}>
+            <h2 style={{overflow: 'visible', whiteSpace: 'pre',
+fontSize: '46px', letterSpacing: '-2px', color: '#333', lineHeight: '1.1', fontWeight: 700, fontStyle: 'normal', fontFamily: 'Arvo, serif', margin:0, }}>
+  Create An Account
+  </h2>
+    <Typography variant='caption'>
+      Please fill up all fields in this form
+    </Typography>
+    
+    <Grid align='center'>
+    <form>
+      <TextField fullwidth label="Enter Your Name:" />
+      <TextField fullwidth label="Email Address" />
+      <TextField fullwidth label="Create Password" />
+      <TextField fullwidth label="Confirm Password" />
+
+      <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>}
+      >
+         Send
+      </Button>
+    
+    </form>
+    </Grid>
+          </Grid>
         </Paper>
       </Grid>
     </Router>
